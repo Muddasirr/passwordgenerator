1f99c2a1eade3c5647687ce5224b013115a8f6d9
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-01-31 16:38:22 +0500

    commit

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -2,8 +2,15 @@ import {useContext, createContext, useState} from 'react';
 
 
 export const TodoContext = createContext({
+todos:[
+    {id:1,todo:"Todo msg",completed:false
+}
+],
+addTodo:(todo) => {},
+updatedTodo:(id,todo)=>{},
+deleteTodo:(id)=>{},
+toggleComplete:(id)=>{}
 
-    
 });
 
 export const useTodo =()=>{
