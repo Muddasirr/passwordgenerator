c2dc8bb094e45e091d58f19637b0baaa17dd9cad
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-01-31 16:38:28 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
-import ThemeBtn from "./components/ThemeBtn";
-import { ThemeProvider } from "./contexts/theme";
-import {useEffect,useState} from "react"
-import Card from "./components/Card";
+import { TodoProvider } from "./contexts";
+
 
 
 function App() {
 
+const [todos, setTodos] = useState([])
 
+const addTodo = (todo) => {
+    setTodos([...todos, {id:Date.now(),...todo}])
+}
+const updateTodo=(id,todo)=>{
+    setTodos((prevTodos)=>prevTodos.map((item)=>item.id===id?todo:item))
+}
 
+const deleteTodo=(id)=>{
+setTodos((prev)=>prev.filter((item)=>item.id!==id))
 
+}
+const toggleComplete=(id)=>{
+    setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id===id?
+    {...prevTodo,completed:!completed}
+    :prevTodo))
+}
 
   return(
- <div>
+ <TodoProvider value={{todos,addTodo,updatedTodo,
+ deleteTodo,toggleComplete}}>
 
-    
- </div>
+<div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+
+ </TodoProvider>
   )
     
  
