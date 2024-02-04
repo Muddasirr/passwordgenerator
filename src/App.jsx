5c6f22054bc5eba52e0ca968a0329203b9ebba4c
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-02-04 19:43:48 +0500

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { TodoProvider } from "./contexts";
+import TodoForm from "./components/TodoForm";
 
-
+import { useState,useEffect } from "react";
+import { TodoContext } from "./contexts";
+import TodoItem from "./components/TodoItem";
 
 function App() {
 
@@ -9,7 +12,7 @@ const [todos, setTodos] = useState([])
 const addTodo = (todo) => {
     setTodos([...todos, {id:Date.now(),...todo}])
 }
-const updateTodo=(id,todo)=>{
+const updatedTodo=(id,todo)=>{
     setTodos((prevTodos)=>prevTodos.map((item)=>item.id===id?todo:item))
 }
 
@@ -17,11 +20,25 @@ const deleteTodo=(id)=>{
 setTodos((prev)=>prev.filter((item)=>item.id!==id))
 
 }
-const toggleComplete=(id)=>{
-    setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id===id?
-    {...prevTodo,completed:!completed}
-    :prevTodo))
-}
+const toggleComplete = (id) => {
+    setTodos((prev) =>
+        prev.map((prevTodo) =>
+            prevTodo.id === id ?    { ...prevTodo, completed: !prevTodo.completed } : prevTodo
+        )
+    );
+};
+
+useEffect(() => {
+    const todos=JSON.parse(localStorage.getItem("todos")) 
+    if(todos && todos.length>0){
+        setTodos(todos)
+    
+    }
+},[])
+
+useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+},[todos])
 
   return(
  <TodoProvider value={{todos,addTodo,updatedTodo,
@@ -31,16 +48,24 @@ const toggleComplete=(id)=>{
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                         <div key={todo.id}
+                         className="w-full">
+                            <TodoItem todo={todo}/>
+
+
+                         </div>   
+                        ))}
                     </div>
                 </div>
             </div>
 
  </TodoProvider>
-  )
+  ) 
     
  
 }
